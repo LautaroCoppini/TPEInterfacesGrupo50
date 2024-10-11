36689ba1b238bfd3c38f157b523721c94a9fb3e0
Lucas Manuel Ferreiro Zunino <lferreiro.823@gmail.com>
2024-10-11 17:44:36 -0300

    Boton jugar animado

--- TP2/EntregaFinal/js/loading.js
+++ TP2/EntregaFinal/js/loading.js
@@ -3,23 +3,17 @@
 let progress = 0;
         const progressText = document.getElementById("porcentajedeprogreso");
         const body = document.body;
-
-        // Desactiva el scroll
         body.style.overflow = 'hidden';
-        
         const interval = setInterval(() => {
-            progress += 1;
+            progress++;
             progressText.textContent = progress + "%";
-
             if (progress >=100) {
                 clearInterval(interval);
                 setTimeout(cargaCompletada,1000);
-                
             }
         }, 50);
 
 function cargaCompletada(){
                 document.getElementById("pantalladecarga").style.display = "none";
-                // Activa el scroll nuevamente
                 body.style.overflow = 'auto';
 }
