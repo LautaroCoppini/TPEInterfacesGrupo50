32d9faf6de23fc3346b8a8fdf7f99d81892d051a
Lucas Manuel Ferreiro Zunino <lferreiro.823@gmail.com>
2024-11-16 10:01:35 -0300

    hola

---
+++ TP4/EntregaFinal/js/menuHamburguesa.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let menuHamburguesaBtn = document.getElementById("menuHamburguesa");
+let menuHamburguesaPos1 = document.getElementById("menuHamburguesaPos1");
+let menuHamburguesaPos2 = document.getElementById("menuHamburguesaPos2");
+let menuHamburguesaPos3 = document.getElementById("menuHamburguesaPos3");
+
+menuHamburguesaBtn.addEventListener("click", ()=>{
+    
+})
