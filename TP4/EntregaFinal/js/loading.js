9650990c70ab2269c2c134e586c3ffc498f5b7e4
Lautaro Coppini <lautarocoppini@gmail.com>
2024-11-16 16:51:45 -0300

    Cambios minimos en laapp+

--- TP4/EntregaFinal/js/loading.js
+++ TP4/EntregaFinal/js/loading.js
@@ -1,6 +1,7 @@
 'use strict';
 document.addEventListener("DOMContentLoaded", () => {
     let personajes = document.querySelectorAll(".personajeCarga");
+    let progreso = document.getElementById("progreso");
     console.log(personajes)
     let progress = 0;
             const progressText = document.getElementById("porcentajedeprogreso");
@@ -9,6 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
             let personajeActual = 0;
             const interval = setInterval(() => {
                 progress++;
+                //progreso.style.width = progress + "%";
                 personajeActual = Math.floor(progress/10);
                 if(personajeActual > 0){
                     personajes[personajeActual-1].style.display = "none";
@@ -16,9 +18,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 if(personajeActual<10){
                     personajes[personajeActual].style.display = "block";
                 }else{
-                    personajes[9].style.display = "block";
+                    personajes[10].style.display = "block";
                 }
-                console.log(Math.floor(progress/10))
                 progressText.textContent = progress + "%";
                 if (progress >=100) {
                     clearInterval(interval);
