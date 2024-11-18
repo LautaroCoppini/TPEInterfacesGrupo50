8ad983a2539e4bba3c28d741ee44fd488464c0e1
Lucas Manuel Ferreiro Zunino <lferreiro.823@gmail.com>
2024-11-18 16:32:08 -0300

    Se hizo el modelo3d

--- TP4/EntregaFinal/js/parallaxMouse.js
+++ TP4/EntregaFinal/js/parallaxMouse.js
@@ -4,13 +4,20 @@ document.addEventListener("mousemove", parallax);
 
 let imagen = document.querySelector(".parallaxMouse");
 
+let model = document.getElementById('model');
+
+let rect = model.getBoundingClientRect();
+
+
 function parallax(e) {
-    let _w = window.innerWidth / 2;
-    let _h = window.innerHeight / 2;
-    let _mouseX = e.clientX;
-    let _mouseY = e.clientY;
+    let w = window.innerWidth / 2;
+    let h = window.innerHeight / 2;
+    let mouseX = e.clientX;
+    let mouseY = e.clientY;
+    let depth1 = `${-100 - (mouseX - w) * 0.01}px ${0 - (mouseY - h) * 0.01}px`;
+    imagen.style.translate = depth1;
 
-    let _depth1 = `${-100 - (_mouseX - _w) * 0.01}px ${0 - (_mouseY - _h) * 0.01}px`;
-
-    imagen.style.translate = _depth1;
+    let valor1 = Math.round(mouseX / rect.width * 70)*-1;
+    let valor2 = (Math.round(window.innerHeight / rect.height * 90) - Math.round(   mouseY / rect.height * 90))+45;
+    model.cameraOrbit = `${valor1}deg ` + `${valor2}deg auto`;
 }
